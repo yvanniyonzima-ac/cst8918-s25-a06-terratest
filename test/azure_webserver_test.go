44688802793b0276fd6b6aaf13cac456e63c87d7
yvanniyonzima-ac <niyo0041@algonquinlive.com>
2025-06-14 15:25:20 -0400

    added tests to confirm NIC exists, NIC is connected to the VM and Ubuntu version is correct

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "bd7bd383-5df6-4911-a820-52a7dc9cdb5b"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "niyo0041",
 		},
 	}
 
@@ -31,6 +31,25 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-	// Confirm VM exists
-	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+	t.Run("Confirm VM exists", func(t *testing.T) {
+		assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+	})
+
+	t.Run("Confirm NIC exists", func(t *testing.T) {
+		nicName := terraform.Output(t, terraformOptions, "nic_name")
+		assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID))
+	})
+
+	t.Run("Confirm NIC is connected to the VM", func(t *testing.T) {
+		nicName := terraform.Output(t, terraformOptions, "nic_name")
+		vmNicsList := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+		assert.Contains(t, vmNicsList, nicName, "The NIC should be connected to the VM")
+	})
+
+	t.Run("Confirm correct Ubuntu version", func(t *testing.T) {
+		expectedSKU := "22_04-lts-gen2"
+		vmImageStruct := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
+		assert.Equal(t, expectedSKU, vmImageStruct.SKU, "The VM should be running the correct Ubuntu version")
+	})
+
 }
